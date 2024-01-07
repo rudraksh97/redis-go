54742c172dd5810e8936f0b37612007dcb485718
Rudraksh Agarwal <rudrakshiitd@outlook.com>
2024-01-07 23:59:41 +0530

    feat: add a basic tcp srvr which runs at 6379 port

--- main.go
+++ main.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	for {
+		deserializedMessage, _ := readAndDeserialize(reader)
+		switch deserializedMessage {
+		case "PING":
+			conn.Write([]byte(serializeSimpleString("PONG")))
+		case "ECHO":
+			conn.Write([]byte(serializeSimpleString("Tujha Aaichi Gaand")))
+		default:
+			conn.Write([]byte(serializeError("ERR unknown command")))
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Entry point")
+	ln, err := net.Listen("tcp", ":6379")
+
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		return
+	}
+	defer ln.Close()
+
+	fmt.Println("Listening on :6379 for connections...")
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			return
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func readAndDeserialize(reader *bufio.Reader) (any, error) {
+	message, err := getMessage(reader)
+
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+		return nil, err
+	}
+
+	deserializedMessage, err := deserialize(message)
+
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+		return nil, err
+	}
+
+	return deserializedMessage, nil
+}
+
+// func getCompleteMessage(reader *bufio.Reader) (string, error) {
+// 	message, err := getMessage(reader)
+// }
+
+func getMessage(reader *bufio.Reader) (string, error) {
+	message, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+		return "", err
+	}
+
+	fmt.Printf("Command received: %s", message)
+
+	trimmedMessage := strings.TrimSpace(message)
+	return trimmedMessage, nil
 }
